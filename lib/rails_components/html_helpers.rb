d006fc165cfdd9e5826a30d34e91e68abe1a1416
Peter Schilling <peter@peterschilling.org>
2016-09-24 13:57:49 -0700

    Support different merge strategies in html

--- lib/rails_components/html_helpers.rb
+++ lib/rails_components/html_helpers.rb
@@ -2,7 +2,18 @@
 module RailsComponents
   module HtmlHelpers
     def html(html_attributes_to_merge = {})
-      merge(html_attributes_to_merge) { |key, v1, v2| [v1, v2].flatten(1) }
+      strategy = delete(:html_merge_strategy) || :combine
+
+      case strategy.to_sym
+      when :combine
+        merge(html_attributes_to_merge) { |key, v1, v2| [v1, v2].flatten(1) }
+      when :merge
+        html_attributes_to_merge.merge(self)
+      when :replace
+        self
+      else
+        raise "unknown html_merge_stragey: #{strategy}"
+      end
     end
   end
 end
